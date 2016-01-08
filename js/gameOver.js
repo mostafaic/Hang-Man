dc76fce6ba47419b6e1e77f1e38ef9f57ac41928
mostafa <mostafa@localhost.localdomain>
2016-01-08 22:22:15 +0200

    New Function

---
+++ js/gameOver.js
@@ -0,0 +1,18 @@
+function showpopup(){
+	var off= document.getElementById('sleep');
+	var pop=document.getElementById('Gover');
+	off.style.display='block';
+	pop.style.display='block';
+
+    var score = document.getElementById('fData');
+    sore=23;
+}
+
+function retryGame(){
+      alert('Rest the whole game');
+}
+
+function test(){
+showpopup()
+}
+
