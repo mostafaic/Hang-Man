2468b59d9052ad972f3d5af3f10cb8deda53869e
Hazem Taha <thatskan@gmail.com>
2016-01-10 23:32:16 +0200

    added level 1 words

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -11,7 +11,14 @@ var playerObj = {
 		'noHint':false
 	}
 }
-var lvl1Words = [{'word':'','desc':'','hint':''}];
+var lvl1Words = [{'word':'Great','desc':'Extra Good','hint':''},{'word':'Clean','desc':'Tidy and dust free','hint':''},
+				{'word':'Milk','desc':'Dairy Drink','hint':''},{'word':'Head','desc':'Body Important Part','hint':''},
+				{'word':'Strange','desc':'Unrecognized and not clear','hint':''},{'word':'Palace','desc':'Big house','hint':''},
+				{'word':'Chew','desc':'One of the eating proccess','hint':''},{'word':'Mimic','desc':'to imitate someone','hint':''},
+				{'word':'Corn','desc':'Cinema favorite habit','hint':''},{'word':'Sponge','desc':'Used to clean things','hint':''},
+				{'word':'Tribe','desc':'A group of people','hint':''},{'word':'Fence','desc':'What surround propreties','hint':''},
+				{'word':'Shred','desc':'Cut into pieces','hint':''},{'word':'Dread','desc':'Horribaly bad','hint':''},
+				{'word':'Crumble','desc':'Fail apart','hint':''}];
 var lvl2Words = [{'word':'','desc':'','hint':''}];
 var lvl3Words = [{'word':'','desc':'','hint':''}];
 var wordsPicked = [];
