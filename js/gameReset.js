0aa86047aa3a069eb24326d781f52ade59f907ae
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-01-12 17:49:30 +0200

    last update for gamereset ayad

--- js/gameReset.js
+++ js/gameReset.js
@@ -1,29 +1,33 @@
 	function gameReset(mode)
     {
-    	//var x;  // el global variable bta3t el level
-    	// var score = document.getElementById('score');
-		var wrongLetter = document.getElementById('listOfWrongLetters');
+    	var wrongLetter = document.getElementById('listOfWrongLetters');
 
 		if (mode == 0){    //game over mode  
 			
 
 			playerObj.score = 0;
-			// score.innerHTML ='0';
 			wrongLetter.innerHTML ='';
 			levelUp(1);
-			drawLetter();
+			drawLetters();
 			getWord();
 
 		}
 
-		if (mode == 1){   //turn to new level
+		if (mode == 1){   //turn to new word
 
 			wrongLetter.innerHTML ='';
-			levelUp(playerObj.lvl);
-			drawLetter();			
+			drawLetters();			
 			getWord();	
 
 		}
+
+		if (mode == 2 ){		//turn to new level
+ 
+			wrongLetter,innerHTML='';
+			levelUp(playerObj.lvl);
+			drawLetters();
+			getWord();
+		} 
 	}	
 
 			
