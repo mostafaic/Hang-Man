4e51da27b310747b9848addc106395361fc0b8e3
Hazem Taha <thatskan@gmail.com>
2016-01-12 16:38:47 +0200

    edited getword

--- js/getWord.js
+++ js/getWord.js
@@ -13,14 +13,14 @@ function getWord(arrWords){
 	var letter,description;
 	
 	var wordCounter=0;
-	var lvl1Words = [{'word':'Great','desc':'Extra Good','hint':''},{'word':'Clean','desc':'Tidy and dust free','hint':''},
-				{'word':'Milk','desc':'Dairy Drink','hint':''},{'word':'Head','desc':'Body Important Part','hint':''},
-				{'word':'Strange','desc':'Unrecognized and not clear','hint':''},{'word':'Palace','desc':'Big house','hint':''},
-				{'word':'Chew','desc':'One of the eating proccess','hint':''},{'word':'Mimic','desc':'to imitate someone','hint':''},
-				{'word':'Corn','desc':'Cinema favorite habit','hint':''},{'word':'Sponge','desc':'Used to clean things','hint':''},
-				{'word':'Tribe','desc':'A group of people','hint':''},{'word':'Fence','desc':'What surround propreties','hint':''},
-				{'word':'Shred','desc':'Cut into pieces','hint':''},{'word':'Dread','desc':'Horribaly bad','hint':''},
-				{'word':'Crumble','desc':'Fail apart','hint':''}];
+	var lvl1Words = [{'word':'great','desc':'Extra Good','hint':''},{'word':'clean','desc':'Tidy and dust free','hint':''},
+				{'word':'milk','desc':'Dairy Drink','hint':''},{'word':'head','desc':'Body Important Part','hint':''},
+				{'word':'strange','desc':'Unrecognized and not clear','hint':''},{'word':'palace','desc':'Big house','hint':''},
+				{'word':'chew','desc':'One of the eating proccess','hint':''},{'word':'mimic','desc':'to imitate someone','hint':''},
+				{'word':'corn','desc':'Cinema favorite habit','hint':''},{'word':'sponge','desc':'Used to clean things','hint':''},
+				{'word':'tribe','desc':'A group of people','hint':''},{'word':'fence','desc':'What surround propreties','hint':''},
+				{'word':'shred','desc':'Cut into pieces','hint':''},{'word':'dread','desc':'Horribaly bad','hint':''},
+				{'word':'crumble','desc':'Fail apart','hint':''}];
 	var lvl2Words = [{'word':'','desc':'','hint':''}];
 	var lvl3Words = [{'word':'','desc':'','hint':''}];	
 	var prevWords=[15];
