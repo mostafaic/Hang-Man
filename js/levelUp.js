e2b4890a6d842c216b787a97caec391712bd5e71
korabieka <hussienashour@ymail.com>
2016-01-14 11:54:21 -0800

    sounds again and fixing some ids

--- js/levelUp.js
+++ js/levelUp.js
@@ -2,12 +2,16 @@ function levelUp(level){
         
         if ( level === 2 )
         {
+            var ohyeah = new Audio("audio/ohyeah.wav");
+            ohyeah.play();
             playerObj.badges.lvl_1= true;
             drawBadges();
         }
         
         else if ( level === 3 )
         {
+            var ohyeah = new Audio("audio/ohyeah.wav");
+            ohyeah.play();
             playerObj.badges.lvl_2= true;
             drawBadges();
         }
@@ -15,6 +19,8 @@ function levelUp(level){
         
         else if ( level === 4 )
         {
+           var ohyeah = new Audio("audio/ohyeah.wav");
+           ohyeah.play();
            playerObj.badges.lvl_3= true;
             drawBadges();
         }
